4e1193c8b9fa04afd2d24265b19842eb86b95c10
Feng <630065152@qq.com>
2018-03-07 16:56:49 +0800

    star组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -33,7 +33,23 @@
         </div>
         <div v-show='detailShow' class='detail'>
             <div class='detail-wrapper clearfix'>
-                <div class='detail-wrapper'></div>
+                <div class='detail-main'>
+                    <h1 class='name'>{{seller.name}}</h1>
+                    <div class="star-wrapper">
+                        <star :size='48' :score='seller.score' ></star>
+                    </div>
+                    <div class="title">
+                        <div class='line'></div>
+                        <div class='text'>优惠信息</div>
+                        <div class='line'></div>
+                    </div>
+                    <ul v-if='seller.supports'>
+                        <li v-for="(item,index) in seller.supports" class='support-item'>
+                            <span class='icon' :class='classMap[seller.supports[index].type]'></span>
+                            <div class='text'>{{seller.supports[index].description}}</div>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class='detail-close'>
                 <i class='icon-close'></i>
@@ -44,6 +60,7 @@
 </template>
 
 <script>
+import star from '../../components/star/star'
 export default {
     data(){
         return{
@@ -59,6 +76,9 @@ export default {
     created(){
         this.classMap=['decrease','discount','special','invoice','guarantee'];
     },
+    components:{
+        star
+    },
     methods:{
         showDetail(){
             this.detailShow = true;
@@ -70,7 +90,7 @@ export default {
 <style lang='stylus' rel='stylesheet/stylus'>
 @import "../../common/stylus/mixin.styl";
 .header
-    position :relative
+    position :relative 
     color :#ffffff
     overflow :hidden
     background:rgba(7,17,27,0.2)
@@ -189,10 +209,33 @@ export default {
         overflow :auto
         background :rgba(7,17,27,0.8)
         .detail-wrapper
+            width:100%
             min-height :100%  
             .detail-main
                 margin-top :64px
-                padding-bottom :64px 
+                padding-bottom :64px
+                .name
+                    font-size :16px
+                    line-height :16px
+                    text-align :center
+                    font-weight :700
+                .star-wrapper
+                    margin-top :18px
+                    padding:2px 0
+                    text-align:center
+                .title
+                    display :flex
+                    width :80%
+                    margin :28px auto 24px auto
+                    .line
+                        flex: 1
+                        position  :relative
+                        top:-6px
+                        border-bottom :1px solid rgba(255,255,255,0.2)
+                    .text
+                        padding : 0 12px
+                        font-size :14px
+                        font-weight :700
         .detail-close
             position :relative
             width :32px
@@ -200,6 +243,5 @@ export default {
             margin:-64px auto;
             font-size :32px
             clear :both
-
 </style>
 
---
+++ src/components/star/star.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class='star' :class='starType'>
+        <span v-for='item in items' :class="item" class='star-item'> </span>
+    </div>
+</template>
+
+<script>
+    const LENGTH = 5 ;
+    const CLS_on = 'on';
+    const CLS_HALF = 'half';
+    const CLS_OFF = 'off';
+export default {
+    props:{
+        size:{
+            type:Number
+        },
+        score:{
+            type:Number
+        }
+    },
+    computed:{
+        starType(){
+            return 'star-'+this.size;
+        },
+        items(){
+            let result = [];
+            let score = Math.floor(this.score*2) / 2;
+            let hasDecimal = score %1 !== 0;//检查是否是小数
+            let integer = Math.floor(score);
+            // 循环整数，往result内放全星
+            for(let i = 0;i < integer;i++){
+                result.push(CLS_on);
+            }
+            //如果有小数，则有半星，往result内加半星
+            if(hasDecimal){
+                result.push(CLS_HALF);
+            }
+            // 如果长度小于5，往数组内加入暗星
+            while(result.length < 5){
+                result.push(CLS_OFF);
+            }
+            return result;
+        }
+    }
+    
+}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+@import "../../common/stylus/mixin.styl";
+.star
+    font-size :0
+    .star-item
+        display :inline-block
+    &.star-48
+        .star-item
+            width :20px
+            height :20px
+            margin-right :22px
+            background-size :cover
+            &:last-child
+                margin :0
+            &.on
+                bg-image("../../../static/images/star48_on")
+            &.half
+                bg-image("../../../static/images/star48_half")
+            &.off
+                bg-image("../../../static/images/star48_off")
+    &.star-36
+        .star-item
+            width :15px
+            height :15px
+            margin-right :16px
+            background-size :cover
+            &:last-child
+                margin :0
+            &.on
+                bg-image("../../../static/images/star36_on")
+            &.half
+                bg-image("../../../static/images/star36_half")
+            &.off
+                bg-image("../../../static/images/star36_off")
+    &.star-24
+        .star-item
+            width :10px
+            height :10px
+            margin-right :3px
+            background-size :cover
+            &:last-child
+                margin :0
+            &.on
+                bg-image("../../../static/images/star24_on")
+            &.half
+                bg-image("../../../static/images/star24_half")
+            &.off
+                bg-image("../../../static/images/star24_off")
+</style>
